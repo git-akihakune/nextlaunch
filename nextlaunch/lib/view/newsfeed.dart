2c280ee52411db0a37c850f4c493d3cc0c3fda13
git-akihakune <akihakune@protonmail.ch>
2022-07-06 17:53:03 +0700

    Add newsfeed app bar

--- nextlaunch/lib/view/newsfeed.dart
+++ nextlaunch/lib/view/newsfeed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:glassmorphism_widgets/glassmorphism_widgets.dart';
 
 class NewsFeedView extends StatefulWidget {
   const NewsFeedView({Key? key}) : super(key: key);
@@ -11,9 +12,25 @@ class _NewsFeedViewState extends State<NewsFeedView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: NeumorphicText('News Feed'),
+      appBar: GlassAppBar(
+        blur: -10,
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        title: const GlassText(
+          'News',
+          fontWeight: FontWeight.bold,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search_rounded),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
       ),
+      body: const Center(child: Text('NewsFeedView')),
     );
   }
 }
