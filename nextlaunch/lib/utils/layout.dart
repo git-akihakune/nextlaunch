c22076f1f25ca8a4784f68ef4578109d90b3d9c6
git-akihakune <akihakune@protonmail.ch>
2022-07-09 22:42:26 +0700

    Add article time

--- nextlaunch/lib/utils/layout.dart
+++ nextlaunch/lib/utils/layout.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:nextlaunch/utils/models.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'dart:io' show Platform;
 
 class NextLaunchButton extends StatelessWidget implements PreferredSizeWidget {
   @override
@@ -54,5 +58,88 @@ class NextLaunchAppBar extends StatelessWidget implements PreferredSizeWidget {
         )),
       ],
     );
+  }
+}
+
+class ArticleCard extends StatelessWidget {
+  final Article article;
+  const ArticleCard({Key? key, required this.article}) : super(key: key);
+
+  void _onPressed() async {
+    final Uri _url = Uri.parse(article.url);
+    if (!await launchUrl(_url)) throw 'Could not launch $_url';
+  }
+
+  String articleDate() {
+    final DateTime _date = DateTime.parse(article.updatedAt);
+    return timeago.format(_date);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 15.0),
+        child: Center(
+          child: NeumorphicButton(
+            onPressed: () => _onPressed(),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${article.newsSite} - ${articleDate()}',
+                            style: const TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                          Text(
+                            article.title,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          if (!(Platform.isAndroid || Platform.isIOS))
+                            Text(
+                              article.summary,
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                              ),
+                            ),
+                        ],
+                      ),
+                    )),
+                    Neumorphic(
+                      style: NeumorphicStyle(
+                        boxShape: NeumorphicBoxShape.roundRect(
+                          BorderRadius.circular(10),
+                        ),
+                        depth: 10,
+                        intensity: 0.5,
+                      ),
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Image.network(
+                          article.imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
