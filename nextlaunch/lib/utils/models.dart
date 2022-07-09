1ed7de508fabb12fbe62ca4ce47665406ec4b87c
git-akihakune <akihakune@protonmail.ch>
2022-07-09 20:59:27 +0700

    Infinite Scroll on NewsFeed

--- nextlaunch/lib/utils/models.dart
+++ nextlaunch/lib/utils/models.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:io' show Platform;
 
 class Article {
   final int id;
@@ -48,22 +50,77 @@ class ArticleCard extends StatelessWidget {
   final Article article;
   const ArticleCard({Key? key, required this.article}) : super(key: key);
 
+  void _onPressed() async {
+    final Uri _url = Uri.parse(article.url);
+    if (!await launchUrl(_url)) throw 'Could not launch $_url';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Neumorphic(
-        style: NeumorphicStyle(
-          boxShape: NeumorphicBoxShape.roundRect(
-            BorderRadius.circular(10),
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 15.0),
+        child: Center(
+          child: NeumorphicButton(
+            onPressed: () => _onPressed(),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            article.newsSite,
+                            style: const TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            article.title,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          if (!(Platform.isAndroid || Platform.isIOS))
+                            Text(
+                              article.summary,
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                              ),
+                            ),
+                        ],
+                      ),
+                    )),
+                    Neumorphic(
+                      style: NeumorphicStyle(
+                        boxShape: NeumorphicBoxShape.roundRect(
+                          BorderRadius.circular(10),
+                        ),
+                        depth: 10,
+                        intensity: 0.5,
+                      ),
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Image.network(
+                          article.imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-          depth: 10,
-          intensity: 0.5,
-        ),
-        child: const Center(
-            child: Card(
-                child: SizedBox(
-          width: 300,
-          height: 100,
-          child: Center(child: Text('Elevated Card')),
-        ))));
+        ));
   }
 }
